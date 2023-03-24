8009a33c562bbd6fc1c767d23cd420b3e8fca639
CodeBoy722 <moforemmanuel722@gmail.com>
2023-03-24 15:59:53 +0100

    media library

--- app/src/main/java/com/codeboy/mediafacerkotlin/musicSession/MediaLibrary.kt
+++ app/src/main/java/com/codeboy/mediafacerkotlin/musicSession/MediaLibrary.kt
@@ -1,12 +1,15 @@
 package com.codeboy.mediafacerkotlin.musicSession
 
+import android.Manifest
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent.FLAG_IMMUTABLE
 import android.app.PendingIntent.FLAG_UPDATE_CURRENT
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import androidx.core.app.TaskStackBuilder
@@ -250,6 +253,20 @@ class MediaLibrary : MediaLibraryService() {
                     )
                     .setPriority(NotificationCompat.PRIORITY_DEFAULT)
                     .setAutoCancel(true)
+            if (ActivityCompat.checkSelfPermission(
+                    this@MediaLibrary,
+                    Manifest.permission.POST_NOTIFICATIONS
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return
+            }
             notificationManagerCompat.notify(NOTIFICATION_ID, builder.build())
         }
     }
